fa34ab76bcb28e084247a70ced00c28878b1d787
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-06-16 23:22:11 +0900

    プロジェクト ファイルを追加します。

---
+++ PwpFaster/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace PowerDevide {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lv = new System.Windows.Forms.ListView();
+            this.btn_del = new System.Windows.Forms.Button();
+            this.btn_ex = new System.Windows.Forms.Button();
+            this.bar = new System.Windows.Forms.Label();
+            this.pro = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lv
+            // 
+            this.lv.AllowDrop = true;
+            this.lv.CheckBoxes = true;
+            this.lv.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lv.HideSelection = false;
+            this.lv.Location = new System.Drawing.Point(12, 12);
+            this.lv.Name = "lv";
+            this.lv.Size = new System.Drawing.Size(776, 366);
+            this.lv.TabIndex = 0;
+            this.lv.UseCompatibleStateImageBehavior = false;
+            this.lv.View = System.Windows.Forms.View.Details;
+            this.lv.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.lv.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            // 
+            // btn_del
+            // 
+            this.btn_del.Font = new System.Drawing.Font("游ゴシック", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_del.Location = new System.Drawing.Point(12, 397);
+            this.btn_del.Name = "btn_del";
+            this.btn_del.Size = new System.Drawing.Size(360, 40);
+            this.btn_del.TabIndex = 1;
+            this.btn_del.Text = "リストから削除";
+            this.btn_del.UseVisualStyleBackColor = true;
+            this.btn_del.Click += new System.EventHandler(this.btn_del_Click);
+            // 
+            // btn_ex
+            // 
+            this.btn_ex.Font = new System.Drawing.Font("游ゴシック", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ex.Location = new System.Drawing.Point(416, 397);
+            this.btn_ex.Name = "btn_ex";
+            this.btn_ex.Size = new System.Drawing.Size(360, 40);
+            this.btn_ex.TabIndex = 2;
+            this.btn_ex.Text = "実行";
+            this.btn_ex.UseVisualStyleBackColor = true;
+            this.btn_ex.Click += new System.EventHandler(this.btn_ex_Click);
+            // 
+            // bar
+            // 
+            this.bar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.bar.Font = new System.Drawing.Font("Yu Gothic UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.bar.ForeColor = System.Drawing.Color.White;
+            this.bar.Location = new System.Drawing.Point(8, 354);
+            this.bar.Name = "bar";
+            this.bar.Size = new System.Drawing.Size(799, 24);
+            this.bar.TabIndex = 3;
+            this.bar.Text = "PowerPointファイルをドラッグアンドドロップしてください";
+            // 
+            // pro
+            // 
+            this.pro.BackColor = System.Drawing.Color.White;
+            this.pro.Location = new System.Drawing.Point(7, 389);
+            this.pro.Name = "pro";
+            this.pro.Size = new System.Drawing.Size(800, 5);
+            this.pro.TabIndex = 4;
+            this.pro.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 466);
+            this.Controls.Add(this.pro);
+            this.Controls.Add(this.bar);
+            this.Controls.Add(this.btn_ex);
+            this.Controls.Add(this.btn_del);
+            this.Controls.Add(this.lv);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ぱわぽふぁすたー";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pro)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv;
+        private System.Windows.Forms.Button btn_del;
+        private System.Windows.Forms.Button btn_ex;
+        private System.Windows.Forms.Label bar;
+        private System.Windows.Forms.PictureBox pro;
+    }
+}
+
---
+++ PwpFaster/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PowerDevide {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            this.Width = ぱわぽふぁすたー.Properties.Settings.Default.sizex;
+            this.Height = ぱわぽふぁすたー.Properties.Settings.Default.sizey;
+            SetCtrl();
+            lv.Columns.Add("名前");
+            lv.Columns.Add("パス");
+            pro.Width = 0;
+        }
+
+        private void SetCtrl() {
+            int w = this.Width - 15;
+            int h = this.Height - 30;
+            int s = 12;
+            int btn_w = (w - 3 * s) / 2;
+            int btn_h = 40;
+
+            btn_ex.Width = btn_w;
+            btn_del.Width = btn_w;
+            btn_ex.Height = btn_h;
+            btn_del.Height = btn_h;
+            lv.Width = w - s * 2;
+            lv.Height = h - (s * 6 + btn_h);
+            bar.Width = w;
+            bar.Height = s * 2;
+            pro.Height = 3;
+
+            lv.Top = s;
+            lv.Left = s;
+            int btn_top = lv.Height + 2 * s;
+            btn_del.Top = btn_top;
+            btn_del.Left = s;
+            btn_ex.Top = btn_top;
+            btn_ex.Left = w - (btn_w + s);
+            bar.Top = h - 2 * s - 9;
+            bar.Left = 0;
+            pro.Top = h - 12;
+            pro.Left = 0;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e) {
+            SetCtrl();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e) {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            ListViewItem lvi;
+            for (int i = 0; i < files.Length; i++) {
+                string ext = Path.GetExtension(Path.GetFileName(files[i]));
+                if (ext == ".pptx" || ext == ".pptm" || ext == ".ppt") {
+                    lvi = lv.Items.Add(Path.GetFileName(files[i]));
+                    lvi.SubItems.Add(files[i]);
+                }
+            }
+            foreach (ColumnHeader ch in lv.Columns) {
+                ch.Width = -1;
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e) {
+            // ドラッグされモノがファイル以外は何もしない
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void btn_ex_Click(object sender, EventArgs e) {
+            int goal = 4 * lv.Items.Count;
+            int now = 0;
+            foreach (ListViewItem lvi in lv.Items) {
+                ProgressSet(1, ++now, goal);//更新1
+                string inPas = lvi.SubItems[1].Text;//pptファイルフルパス
+                string outPas = lvi.SubItems[1].Text;//pptファイル名フォルダまでのパス(展開で作られる
+                int l = outPas.Length;
+                int lExt = Path.GetExtension(outPas).Length;
+                outPas = outPas.Remove(l - lExt, lExt);//パス長-拡張子長から拡張子長さ分消す
+                try {
+                    ZipFile.ExtractToDirectory(inPas, outPas);
+                }
+                catch (Exception Error) { }
+
+                ProgressSet(2, ++now, goal);//更新2
+                string ext = "m4a";
+                string[] files = Directory.GetFiles(outPas + "/ppt/media", "*.m4a");
+                //foreach (string s in files) //MessageBox.Show(s);
+                if (files.Length == 0) {
+                    files = Directory.GetFiles(outPas + "/ppt/media", "*.mp3");
+                    ext = "mp3";
+                }
+                if (files.Length == 0) {
+
+                    files = Directory.GetFiles(outPas + "/ppt/media", "*.wav");
+                    ext = "wav";
+                }
+                try {
+                    foreach (string s in files) {
+                        string name = Path.GetFileName(s);
+                        File.Move(s, outPas + "/" + name);
+                    }
+                }
+                catch (Exception Error) { }
+
+
+                ProgressSet(3, ++now, goal);//更新3
+                string[] folders = Directory.GetDirectories(outPas);//余分なフォルダ削除
+                foreach (string s in folders) Directory.Delete(s, true);
+                files = Directory.GetFiles(outPas + "/", "*.xml");
+                foreach (string s in files) File.Delete(s);
+
+                lv.Items.Remove(lvi);//リストから削除
+
+                ProgressSet(4, ++now, goal);//更新4
+                try {
+                    StreamWriter sw = File.CreateText(outPas + ".m3u");
+
+                    sw.WriteLine("#EXTM3U");
+                    files = Directory.GetFiles(outPas + "/", "*." + ext);
+                    List<(int, string)> fileList = new List<(int, string)>();
+
+                    for (int i = 0; i < files.Length; i++) {
+                        string s = Path.GetFileName(files[i]);
+                        string ns = s.Remove(s.Length - 4, 4);
+                        ns = ns.Remove(0, 5);
+                        int n = Int32.Parse(ns);
+                        fileList.Add((n, s));
+                    }
+                    fileList.Sort();
+
+                    string folderName = Path.GetFileName(outPas);
+                    for (int i = 0; i < files.Length; i++) {
+                        sw.WriteLine(folderName + @"\" + fileList[i].Item2);
+                    }
+                    sw.Close();
+                }
+                catch (Exception Error) { }
+            }
+            ProgressSet(0, 0, 0);//更新0
+        }
+
+        private void btn_del_Click(object sender, EventArgs e) {
+            Del();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Delete) Del();
+        }
+        private void Del() {
+            foreach (ListViewItem lvi in lv.CheckedItems) {
+                lv.Items.Remove(lvi);
+            }
+        }
+
+      
+
+        private void ProgressSet(int n, int now, int goal) {
+            if(n == 0) {
+                bar.Text = "PowerPointファイルをドラッグアンドドロップしてください";
+                pro.Width = 0;
+            }
+            else {
+                switch (n) {
+                    case 1:
+                        bar.Text = "展開中";
+                        break;
+                    case 2:
+                        bar.Text = "音声ファイル移動中";
+                        break;
+                    case 3:
+                        bar.Text = "余分なファイルを削除中";
+                        break;
+                    case 4:
+                        bar.Text = "プレイリスト生成中";
+                        break;
+                }
+                bar.Text = bar.Text + " 現在" + (now / 4).ToString() + "/" + (goal / 4).ToString();
+                int w = this.Width;
+                pro.Width = w / goal * now;
+            }
+            Refresh();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            save();
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            save();
+        }
+        private void save() {
+            ぱわぽふぁすたー.Properties.Settings.Default.sizex = this.Width;
+            ぱわぽふぁすたー.Properties.Settings.Default.sizey = this.Height;
+        }
+    }
+}
